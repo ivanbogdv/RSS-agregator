297cdc4f9512fac3cd34b53654cd7063483a20d6
ivanbogdv <ivan.bogdv@gmail.com>
2023-07-15 15:55:31 +0300

    Fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
   module: {
     rules: [
       {
