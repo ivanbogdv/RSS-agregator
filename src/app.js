dd5d4a1eca74fd4077f0755ddf317d2fe1f48da4
ivanbogdv <iavn.bogdv@gmail.com>
2023-07-16 15:08:05 +0300

    Change index.html

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import { string, setLocale } from 'yup';
+import onChange from 'on-change';
+
+export default () => {
+  console.log('!test!');
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './styles.scss';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import app from './app.js';
+
+app();
