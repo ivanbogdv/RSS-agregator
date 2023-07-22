c850c5c46d963021103bf7eebe1b27075c0a30d5
ivanbogdv <ivan.bogdv@gmail.com>
2023-07-22 18:59:56 +0300

    Add validation with yup

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 [![Actions Status](https://github.com/ivanbogdv/frontend-project-11/workflows/hexlet-check/badge.svg)](https://github.com/ivanbogdv/frontend-project-11/actions) [![Maintainability](https://api.codeclimate.com/v1/badges/22471cd73294305f0f15/maintainability)](https://codeclimate.com/github/ivanbogdv/frontend-project-11/maintainability)
 
 
-https://vercel.com/ivanbogdv/frontend-project-11
+https://frontend-project-11-eight-chi.vercel.app/
--- src/app.js
+++ src/app.js
@@ -1,6 +1,69 @@
-import { string, setLocale } from 'yup';
 import onChange from 'on-change';
+import * as yup from 'yup';
+// import i18next from 'i18next';
+// import render from './view.js';
+
+const render = (elements) => (path, value) => {
+  elements.input.classList.remove('is-invalid'); // удаляем класс для ввода ошибки
+  elements.feedback.classList.remove('text-success', 'text-danger');
+  elements.feedback.textContent = '';
+
+  switch (path) {
+    case 'urls':
+      elements.feedback.classList.add('text-success');
+      elements.feedback.textContent = 'RSS успешно загружен';
+      break;
+
+    case 'form.url':
+      elements.input.value = value;
+      break;
+
+    case 'form.error':
+      elements.input.classList.add('is-invalid');
+      elements.feedback.classList.add('text-danger');
+      elements.feedback.textContent = value.message;
+      break;
+
+    default:
+      break;
+  }
+};
 
 export default () => {
-  console.log('!test!');
+  const elements = {
+    input: document.querySelector('#url-input'), // ссылка RSS
+    form: document.querySelector('.rss-form'),  // поле ввода
+    feedback: document.querySelector('.feedback'), // текст ошибки
+  };
+
+  const state = onChange(
+    {
+      form: {
+        url: null,
+        error: {},
+      },
+      urls: [],
+    },
+    render(elements)
+  );
+
+  elements.form.addEventListener('submit', (e) => { // обработчик события
+    e.preventDefault(); // Скидываем настройки по умолчанию
+
+    const formData = new FormData(e.target); // Позволяет нам получить данные формы
+    const url = formData.get('url').trim(); // получаем значение поля ввода с именем 'url', trim() удаляем пробелы
+
+    state.form.url = url; // сохраняем полученное значение URL в объект состояния state
+
+    const schema = yup.string().url().notOneOf(state.urls);
+
+    schema // Библиотека Yup
+      .validate(state.form.url)
+      .then(() => {
+        state.urls.push(state.form.url);
+        state.form.url = null;
+      })
+      .catch((error) => (state.form.error = error))
+      .finally(() => elements.input.focus());
+  });
 };
