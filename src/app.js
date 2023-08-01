1809238454c775b5cba7c5c2207a45c119192ef9
ivanbogdv <ivan.bogdv@gmail.com>
2023-08-01 13:53:35 +0300

    moving code from init to app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import i18next from 'i18next';
-import { setLocale } from 'yup';
-import init from './init.js';
+import { string, setLocale } from 'yup';
 import resources from './locales/index.js';
+import axios from 'axios';
+import onChange from 'on-change';
+import render from './view.js';
 
 export default () => {
   const defaultLanguage = 'ru'; // язык по умолчанию из i18next
@@ -19,5 +21,42 @@ export default () => {
       debug: true,
       resources,
     })
-    .then(() => init(i18nInstance));
+    .then(() => {
+      const elements = { // Извлекаем элементы из HTML
+        input: document.querySelector('#url-input'),
+        form: document.querySelector('.rss-form'),
+        exampleLink: document.querySelector('p.mt-2.mb-0'),
+        feedback: document.querySelector('.feedback'),
+      };
+    
+      const state = onChange( // Состояние
+        {
+          form: {
+            url: null,
+            error: {},
+          },
+          urls: [],
+        },
+        render(elements, i18nInstance)
+      );
+      elements.form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    
+        const formData = new FormData(e.target); // Данные из формы, отправленные пользователем
+        const url = formData.get('url').trim(); // Извлекаем значение введенное пользователем
+    
+        state.form.url = url; // Сохранили данные в переменной
+    
+        const schema = string().url().notOneOf(state.urls); // Проверка на повтор и на URL-адрес
+    
+        schema
+          .validate(state.form.url)
+          .then(() => {
+            state.urls.push(state.form.url); // Добавляем введенный URL-адрес в список
+            state.form.url = null; // Обнуляем поле ввода
+          })
+          .catch((error) => (state.form.error = error)) // Сохраняем ошибку в переменную
+          .finally(() => elements.input.focus()); // Снова фокусируем курсор на поле ввода
+      });
+    });
 };
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,10 +1,13 @@
 export default {
     translation: {
+      posts: 'Посты',
+      feeds: 'Фиды',
       success: 'RSS успешно загружен',
       errors: {
         url: 'Ссылка должна быть валидным URL',
         exist: 'RSS уже существует',
         noRSS: 'Ресурс не содержит валидный RSS',
+        NetworkError: 'Ошибка сети',
       },
     },
   };
